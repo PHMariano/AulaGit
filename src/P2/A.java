294fb0c92fa3402c241d6988858c1d7d2282d212
Paulo H <Paulo H@DESKTOP-H42GACS>
2017-04-25 11:36:31 -0300

    Versão 1 da Programação Modular

---
+++ src/P2/A.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package P2;
+
+/**
+ *
+ * @author Paulo H
+ */
+import P1.*;
+
+public class A {
+
+    private int pri = 1;
+    protected int pro = 2;
+    public int pub = 3;
+    int pac = 4;
+
+    private class X implements Ix {
+
+        public A xpub = new A();
+        private A xpri = new A();
+        protected A xpro = new A();
+        A xpac = new A();
+
+        public void fx() {
+            int soma1 = 0;
+            int soma2 = 0;
+            soma1 = pri + pro + pub + pac;
+            soma2 = xpri.pri + xpro.pro + xpub.pub + xpac.pac;
+            System.out.println("Soma1 = " + soma1);
+            System.out.println("Soma2 = " + soma2);
+        }
+
+    }
+
+    public Ix fa() {
+        return new X();
+    }
+
+}
---
+++ src/Ver1.java
@@ -0,0 +1,24 @@
+
+import P1.*;
+import P2.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Paulo H
+ */
+public class Ver1 {
+
+    static public void main(String[] arg) {
+        A a = new A();
+
+        Ix x;
+        x = a.fa();
+
+        x.fx();
+    }
+}
